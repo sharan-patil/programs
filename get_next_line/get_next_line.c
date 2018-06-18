22cea9ca6970ba81ed8927440e4e35b9cd86a3b2
sharan-patil <sharan-patil>
2018-06-17 18:43:16 -0700

    ..

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -5,41 +5,44 @@
 
 int	get_next_line(const int fd, char **line)
 {
-	char *a;
+	char a[BUFF_SIZE + 1];
+	char *temp;
 	static int i;
 	int bytes_read;
 	int j;
 
+	temp = *line;
 	i = 0;
+
 	while (1)
 	{
+		temp[i] = (char)malloc(sizeof(char) * BUFF_SIZE);
 		j = 0;
-		a = (char*)malloc(sizeof(char) * BUFF_SIZE);
 		bytes_read = read(fd, a, BUFF_SIZE);
+		a[bytes_read] = '\0';
 		if (!bytes_read)
 			return (-1);
 		while (j < BUFF_SIZE)
 		{
-			if (a[i] == '\n' || a[i] == -1)
+			if (a[j] == '\n' || a[j] == -1)
 				return (0);
-			line[0][i] = a[i];
+			line[0][i] = a[j];
 			i++;
 			j++;
 		}
-		free(a);
 	}
 	
 }
 
-// int	main(void)
-// {
-// 	char **line;
-// 	int fd;
+int	main(void)
+{
+	char **line;
+	int fd;
 
-// 	fd = open("text", O_RDWR);
-// 	line = (char**)malloc(1);
-// 	line[0] = (char*)malloc(sizeof(char) * 15);
-// 	get_next_line(fd, line);
-// 	printf("line[0]: %s", line[0]);
-// 	return (0);
-// }
+	fd = open("text", O_RDWR);
+	line = (char**)malloc(1);
+	line[0] = (char*)malloc(sizeof(char) * 15);
+	get_next_line(fd, line);
+	printf("line[0]: %s", line[0]);
+	return (0);
+}
