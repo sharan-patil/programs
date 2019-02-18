692964d0b45031eecb593088ae788180fbab79e1
admin <admin@USCS-Mac134.local>
2019-02-17 18:11:56 -0800

    fixed offset without top-left

--- fillit/fillit.c
+++ fillit/fillit.c
@@ -7,9 +7,7 @@ int	main(int argc, char **argv)
 {
 	t_tetri blocks[4];
 	int fd;
-	char buf[100];
-	char arr[6];
-	char arr2[6];
+	char arr[5];
 	int i;
 	int j;
 	int k;
@@ -17,37 +15,27 @@ int	main(int argc, char **argv)
 	k = 0;
 	j = 0;
 	fd = open(argv[1], O_RDONLY);
-	read(fd, buf, 5);
-	buf[5] = '\0';
-	printf("%s\n", buf);
-	read(fd, buf, 5);
-	buf[5] = '\0';
-	printf("%s\n", buf);
-	read(fd, buf, 5);
-	buf[5] = '\0';
-	printf("%s\n", buf);
-	// while (read(fd, arr, 5))
-	// {
-	// 	i = 0;
-	// 	printf("read: %s\n", arr);
-	// 	while (i < 4)
-	// 	{
-	// 		if (arr[i] == '#')
-	// 		{ // offsets but does not account for most top left block
-	// 			blocks[k].pieceOffset[j][0] = j;
-	// 			blocks[k].pieceOffset[j][1] = i;
-	// 			printf("%d %d\n", j, i);
-	// 		}
-	// 		i++;
-	// 	}
-	// 	j++;
-	// 	if (j == 4)
-	// 	{
-	// 		j = 0;
-	// 		k++;
-	// 		read(fd, 0, 1);
-	// 	}
-	// }
+     while (read(fd, arr, 5))
+     {
+         i = 0;
+         while (i < 4)
+         {
+             if (arr[i] == '#')
+             { // offsets but does not account for most top left block
+                 blocks[k].pieceOffset[j][0] = j;
+                 blocks[k].pieceOffset[j][1] = i;
+                 printf("%d %d\n", j, i);
+             }
+             i++;
+         }
+         j++;
+         if (j == 4)
+         {
+             j = 0;
+             k++;
+             read(fd, arr, 1);
+         }
+     }
 	close(fd);
 	return (0);
 }
